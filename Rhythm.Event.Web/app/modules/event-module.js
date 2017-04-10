2ff2755c28e6e2523de07b2bf2f8b19111759c07
DESKTOP-ND71S99\Amol <amol.thikane@gmail.com>
2017-04-10 22:27:18 +1000

    Added ngRoute Added route config

--- Rhythm.Event.Web/app/modules/event-module.js
+++ Rhythm.Event.Web/app/modules/event-module.js
@@ -1,1 +1,5 @@
-﻿var eventApp = angular.module("eventApp", []);
+﻿var eventApp = angular.module("eventApp", ["ngRoute"]);
+
+eventApp.config(function ($routeProvider) {
+    $routeProvider.when("/events", { template: "<h1>Reached to events</h1>" });
+});
